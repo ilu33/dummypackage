b07d48cdf9dcd6ee674764df923aad5d2e1eafe7
You-Sheng Yang <vicamo@gmail.com>
2019-09-03 12:07:50 +0000

    Remove debian/ from upstream sources
    Signed-off-by: You-Sheng Yang (楊有勝) <vicamo@gmail.com>

--- dummypackage/scripts/cli.py
+++ dummypackage/scripts/cli.py
@@ -1,13 +1,10 @@
-# Skeleton of a CLI
-
-import click
-
+import argparse
 import dummypackage
 
-
-@click.command('dummypackage')
-@click.argument('count', type=int, metavar='N')
-def cli(count):
+def cli():
     """Echo a value `N` number of times"""
-    for i in range(count):
-        click.echo(dummypackage.has_legs)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('count', type=int, metavar='N')
+    args = parser.parse_args()
+    for i in range(args.count):
+        print(dummypackage.has_legs)
--- setup.py
+++ setup.py
@@ -3,23 +3,20 @@ from setuptools import setup, find_packages
 
 
 setup(name='dummypackage',
-      version='0.0.1',
+      version='0.0.2',
       description=u"Skeleton of a Python package",
       long_description="Dummy package",
       classifiers=[],
       keywords='',
       author=u"Iñaki Malerba",
       author_email='inaki@malerba.space',
-      url='https://salsa.debian.org/salsa-ci-package/pipeline',
+      url='https://salsa.debian.org/salsa-ci-team/dummypackage',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'click'
-      ],
       extras_require={
-          'test': ['pytest'],
+          'test': ['pexpect', 'pytest'],
       },
       entry_points="""
       [console_scripts]
